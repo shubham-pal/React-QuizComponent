e9b1ea59fd48049762939557b1dd3d731c8cf2fc
Pal, Shubham <shubham.pal@cvent.com>
2022-03-21 12:09:39 +0530

    state change on button click

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,15 +2,28 @@ import React, { Component } from "react";
 import QuizQuestionButton from "./QuizQuestionButton";
 
 class QuizQuestion extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      incorrectAnswer: false,
+    };
+  }
+
   handleClick(buttonText) {
     if (buttonText === this.props.quiz_question.answer) {
-      this.props.showNextQuestionHandler()
+      this.props.showNextQuestionHandler();
+      this.setState({ incorrectAnswer: false });
+    } else {
+      this.setState({ incorrectAnswer: true });
     }
   }
 
   render() {
     return (
       <main>
+        {this.state.incorrectAnswer ? 
+          <p className="error">Sorry, that's not right.</p> : null
+        }
         <section>
           <p>{this.props.quiz_question.instruction_text}</p>
         </section>
@@ -18,7 +31,11 @@ class QuizQuestion extends Component {
           <ul>
             {this.props.quiz_question.answer_options.map(
               (answer_option, index) => (
-                <QuizQuestionButton clickHandler={this.handleClick.bind(this)} key={index} button_text={answer_option} />
+                <QuizQuestionButton
+                  clickHandler={this.handleClick.bind(this)}
+                  key={index}
+                  button_text={answer_option}
+                />
               )
             )}
           </ul>
