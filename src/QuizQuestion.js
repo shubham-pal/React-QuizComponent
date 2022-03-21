bd32ecb384b856556ed2585b2a55160cd5908583
Pal, Shubham <shubham.pal@cvent.com>
2022-03-21 11:28:10 +0530

    updated quizquestion

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -1,7 +1,13 @@
-import React, { Component } from 'react'
-import QuizQuestionButton from './QuizQuestionButton'
+import React, { Component } from "react";
+import QuizQuestionButton from "./QuizQuestionButton";
 
 class QuizQuestion extends Component {
+  handleClick(buttonText) {
+    if (buttonText === this.props.quiz_question.answer) {
+      this.props.showNextQuestionHandler()
+    }
+  }
+
   render() {
     return (
       <main>
@@ -10,12 +16,16 @@ class QuizQuestion extends Component {
         </section>
         <section className="buttons">
           <ul>
-            <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]} />
+            {this.props.quiz_question.answer_options.map(
+              (answer_option, index) => (
+                <QuizQuestionButton clickHandler={this.handleClick.bind(this)} key={index} button_text={answer_option} />
+              )
+            )}
           </ul>
         </section>
       </main>
-    )
+    );
   }
 }
 
-export default QuizQuestion
+export default QuizQuestion;
